63412b32797ce3d4d1bc4519f381a731c82390b3
nova-lite-os <zakixcheat@gmail.com>
2025-07-30 14:17:58 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ const client = new Client({
     partials: [Partials.Message, Partials.Channel, Partials.Reaction]
 });
 
+
 const triggerWords = ["/b", "/welcome", "/👏"];
 const emojiToReact = "👏";
 
